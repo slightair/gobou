246e209f1bc289b7ec7468721a6dcf48acb2bf17
Tomohiro Moro <arksutite@gmail.com>
2012-05-23 10:09:38 +0900

    twitter のツイートが取れなくなっていたので修正 & 複数行ツイートに対応した

--- modules/twitter.js
+++ modules/twitter.js
@@ -10,14 +10,22 @@ exports.replyPattern = /https?:\/\/twitter.com(\/#!)?(\/[^\/]+\/status\/\d+)/;
 
 exports.reply = function(client, nick, to, result) {
   jsdom.env({
-    html: "http://twitter.com" + result[2],
+    html: "http://mobile.twitter.com" + result[2],
     src: [jQuery],
     done: function(error, window) {
-      var screenName = "@" + window.$("a.screen-name").text();
-      var tweet = window.$("span.entry-content").text();
-      var message = screenName + ": " + tweet;
-      
-      client.notice(to, message);
+      var screenName = window.$("div.username").text();
+      var tweet = window.$("div.tweet-text").text();
+      var lines = tweet.split("\n");
+      if (lines.length > 1) {
+          client.notice(to, screenName + ":");
+          lines.forEach(function(line){
+              if (line == '') line = ' ';
+              client.notice(to, line);
+          });
+      }
+      else {
+          client.notice(to, screenName + ": " + tweet);
+      }
     }
   });
 }
