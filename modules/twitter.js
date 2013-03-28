7d42990db529d3134be534effab3bb85bd0ad39c
slightair <arksutite@gmail.com>
2013-03-28 18:00:03 +0900

    jsdom + jQuery をやめて cheerio をつかうようにした

--- modules/twitter.js
+++ modules/twitter.js
@@ -1,31 +1,35 @@
 ////
 // reply tweet entry-content
 
-var jsdom = require('jsdom');
-var fs = require('fs');
-var jQueryPath = LIBRARY_PATH + 'jquery-1.6.1.min.js';
-var jQuery = fs.readFileSync(jQueryPath).toString();
+var request = require('request');
+var cheerio = require('cheerio');
 
 exports.replyPattern = /https?:\/\/twitter.com(\/#!)?(\/([^\/]+)\/status(es)?\/\d+)/;
 
 exports.reply = function(client, nick, to, result) {
-  jsdom.env({
-    html: "https://mobile.twitter.com" + result[2],
-    src: [jQuery],
-    done: function(error, window) {
-      var screenName = result[3];
-      var tweet = window.$("div#main-content div.tweet-text").text();
-      var lines = tweet.split("\n");
-      if (lines.length > 1) {
-          client.notice(to, screenName + ":");
-          lines.forEach(function(line){
-              if (line == '') line = ' ';
-              client.notice(to, line);
-          });
-      }
-      else {
-          client.notice(to, screenName + ": " + tweet);
-      }
+  var pageURL = "https://mobile.twitter.com" + result[2];
+  
+  request.get({url : pageURL, encoding : null, headers : {"User-Agent" : "gobou"}}, function(err, response, body){
+    if (err) {
+      client.notice(to, 'ページ取得失敗…');
+      return;
+    }
+    
+    $ = cheerio.load(body, {lowerCaseTags:true, xmlMode:true});
+    
+    var screenName = result[3];
+    var tweet = $("div#main-content div.tweet-text").text();
+    var lines = tweet.split("\n");
+    
+    if (lines.length > 1) {
+        client.notice(to, screenName + ":");
+        lines.forEach(function(line){
+            if (line == '') line = ' ';
+            client.notice(to, line);
+        });
+    }
+    else {
+        client.notice(to, screenName + ": " + tweet);
     }
   });
 }
